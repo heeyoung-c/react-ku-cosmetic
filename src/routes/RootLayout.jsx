9edf84adab8b87693cec9c5155770926c47cb03d
heeyoung-c <heeyoungchun.dev@gmail.com>
2023-11-15 16:21:19 +0900

    ✨ feat: 기본 레이아웃을 위한 RootLayout 파일 추가/#2

---
+++ src/routes/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
+
+export default RootLayout
